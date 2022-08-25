5c8d4796bf21f499a53da5675b13695e51d5fb2a
JODA HANI <dan0748@naver.com>
2022-08-25 16:38:33 +0900

    2022_0825 third commit

--- public/js/section3.js
+++ public/js/section3.js
@@ -24,7 +24,7 @@ let imgMap = (id) => {
     let imgs = []
     for (let i = 0; i < 5; i++) {
         console.log(id)
-        let img = `<img src='../img/${id}/0${i + 1}.jpg'>`
+        let img = `<img src='./public//img/${id}/0${i + 1}.jpg'>`
         imgs.push(img)
     }
     let box = `
